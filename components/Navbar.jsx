94f4732dcb7d69ec55c375cb837875dadea32eac
Lily-Lilian <lialsina03@gmail.com>
2022-10-26 19:29:39 +0200

    feat:updated pages

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
     <div className="flex justify-between absolute w-full z-20 py-6 items-center px-20">
       <div className="flex items-center gap-2">
         <div className="h-10 w-10 rounded-full bg-[#486A6F]"></div>
-        <Link href="/"><a className="text-[#486A6F] font-bold text-xl">Buildit</a></Link>
+        <Link href="/">
+          <a className="text-[#486A6F] font-bold text-xl">Buildit</a>
+        </Link>
       </div>
       <nav className="flex space-x-8 justify-center list-none text-[#486A6F]">
         <li>
